53a79c16e602a90f2062f97b50370a94ed9ba4f8
jamesavechives <yanhuangpai@126.com>
2025-03-14 08:29:44 +0800

    update method Createclass

--- x/nft/keeper/msg_server.go
+++ x/nft/keeper/msg_server.go
@@ -52,8 +52,8 @@ func (k Keeper) Send(goCtx context.Context, msg *nft.MsgSend) (*nft.MsgSendRespo
 	return &nft.MsgSendResponse{}, nil
 }
 
-// Createclass implements the MsgServer interface for creating a new Class.
-func (k Keeper) Createclass(goCtx context.Context, msg *nft.MsgCreateclass) (*nft.MsgCreateclassResponse, error) {
+// / CreateClass implements the MsgServer interface for creating a new Class.
+func (k Keeper) CreateClass(ctx context.Context, msg *nft.MsgCreateclass) (*nft.MsgCreateclassResponse, error) {
 	// 1. Validate basic fields
 	if len(msg.Name) == 0 {
 		return nil, nft.ErrEmptyClassName
@@ -68,7 +68,7 @@ func (k Keeper) Createclass(goCtx context.Context, msg *nft.MsgCreateclass) (*nf
 	if err != nil {
 		return nil, errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid sender address (%s)", msg.Sender)
 	}
-	ctx := sdk.UnwrapSDKContext(goCtx)
+
 	// (Optional) If you only allow certain addresses to create classes, do your checks here.
 	// e.g. check if senderAddr is in some whitelist, or if there's a global "class creation" fee, etc.
 
@@ -93,14 +93,6 @@ func (k Keeper) Createclass(goCtx context.Context, msg *nft.MsgCreateclass) (*nf
 	// 5. Save the class to the store using keeper’s `SaveClass`
 	if err := k.SaveClass(ctx, classObj); err != nil {
 		return nil, err
-	}
-
-	// 6. (Optional) Emit an event for analytics or watchers
-	if eventErr := ctx.EventManager().EmitTypedEvent(&nft.EventCreateclass{
-		ClassId: classID,
-		Creator: msg.Sender,
-	}); eventErr != nil {
-		return nil, eventErr
 	}
 
 	// 7. Return a response
